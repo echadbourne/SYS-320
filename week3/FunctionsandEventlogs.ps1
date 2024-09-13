e1f19c303162cfd7962fabcdd56aed379a64e99f
echadbourne <elizabeth.chadbourne@mymail.champlain.edu>
2024-09-13 14:20:34 -0400

    week 3 lab added

---
+++ week3/FunctionsandEventlogs.ps1
@@ -0,0 +1,60 @@
+﻿# Get login and logoff records from Windows Events
+#Get-EventLog -logname System -source Microsoft-Windows-Winlogon
+function getloginlogs($days){
+
+$loginouts = Get-EventLog -logname System -source Microsoft-Windows-Winlogon -After (Get-Date).AddDays(-$days)
+
+$loginoutstable = @() #empty array to fill customly
+for ($i=0; $i -lt $loginouts.Count; $i++){
+
+#Creating event property value
+$event = ""
+if($loginouts[$i].InstanceId -eq "7001") {$event="Logon"}
+if($loginouts[$i].InstanceId -eq "7002") {$event="Logoff"}
+
+#Creating user property value
+$SID = New-Object System.Security.Principal.SecurityIdentifier($loginouts[$i].ReplacementStrings[1])
+
+$user = $SID.Translate([System.Security.Principal.NTAccount])
+
+#Adding each new line (in form of a custom object) to our empty array
+$loginoutstable += [pscustomobject]@{"Time" = $loginouts[$i].TimeGenerated; `
+                                       "Id" = $loginouts[$i].InstanceId; `
+                                    "Event" = $event; `
+                                     "User" = $user.value; `
+                                       }
+} #End of for
+$loginoutstable
+} #end of function
+
+#getloginlogs(15)
+
+
+
+function getonofftime($days){
+
+$stopstarts = Get-EventLog system -After (Get-Date).AddDays(-$days) | ?{$_.EventID -eq 6006 `
+                                                                    -or $_.EventID -eq 6005 }
+
+$stopstartstable = @() #empty array to fill customly
+for ($i=0; $i -lt $stopstarts.Count; $i++){
+
+#Creating event property value
+$event = ""
+if($stopstarts[$i].EventID -eq "6005") {$event="Startup"}
+if($stopstarts[$i].EventID -eq "6006") {$event="Shutdown"}
+
+#Creating user property value
+$user = "System"
+
+#Adding each new line (in form of a custom object) to our empty array
+$stopstartstable += [pscustomobject]@{"Time" = $stopstarts[$i].TimeGenerated; `
+                                       "Id" = $stopstarts[$i].EventId; `
+                                    "Event" = $event; `
+                                     "User" = $user; `
+                                       }
+} #End of for
+$stopstartstable
+} #end of function
+
+getonofftime(15)
---
+++ week3/FunctionsandEventlogs2.ps1
@@ -0,0 +1,11 @@
+﻿. (Join-Path $PSScriptRoot FunctionsandEventlogs.ps1)
+
+clear
+
+$loginoutstable = getloginlogs(15)
+$loginoutstable
+
+#Get shut downs and startups from past 25 days
+
+$stopstartstable = getonofftime(25)
+$stopstartstable
