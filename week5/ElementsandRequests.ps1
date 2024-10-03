01026d0979dcfedd61c61a648e40a43a833ad61a
echadbourne <elizabeth.chadbourne@mymail.champlain.edu>
2024-10-03 13:23:12 -0400

    Elements and Requests Finished

--- week5/ElementsandRequests.ps1
+++ week5/ElementsandRequests.ps1
@@ -4,7 +4,15 @@
 
 #display links as html elements
 
-$scraped_page.links | select href, outerText
+#$scraped_page.links | select href, outerText
 
 
-#$h2s = $scraped_page.AllElements.body.getElements | Select outerText
+$h2s = $scraped_page.parsedhtml.body.getElementsbytagname("h2") | Select outertext
+#$h2s
+
+# Print the innertext of every div element that has the class as "div-1"
+
+$divs1 = $scraped_page.parsedhtml.body.getelementsbytagname("div") | where { `
+$_.getAttributeNode('class').Value -ilike "div-1"} | select innertext
+
+$divs1
