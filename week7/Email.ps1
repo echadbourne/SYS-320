1d232aa90e12fffb8e0522f4ec9a090cc0ab7097
echadbourne <elizabeth.chadbourne@mymail.champlain.edu>
2024-10-18 08:19:55 -0400

    pushing week7

--- week7/Email.ps1
+++ week7/Email.ps1
@@ -4,7 +4,7 @@ $From = "elizabeth.chadbourne@mymail.champlain.edu"
 $To = "elizabeth.chadbourne@mymail.champlain.edu"
 $Subject = "Suspicious Activity"
 
-$Password = "geid drnd geli nhnm" | ConvertTo-SecureString -AsPlainText -Force
+$Password = "" | ConvertTo-SecureString -AsPlainText -Force
 $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $From, $Password
 
 Send-MailMessage -From $From -To $To -Subject $Subject -Body $Body -SmtpServer "smtp.gmail.com" -port 587 -UseSsl -Credential $Credential
