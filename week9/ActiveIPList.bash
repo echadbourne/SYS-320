5e09a34cad19af1652151422d39f8b9de7fcc276
echadbourne <elizabeth.chadbourne@mymail.champlain.edu>
2024-10-24 14:38:26 -0400

    IPListing Compete

---
+++ week9/ActiveIPList.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#list all the ips in the given network prefix
+#/24 only
+
+#Usage: bash IPList.bash 10.0.17
+[ "$#" -lt 1 ] &&  echo "Usage: $0 <Prefix>" && exit 1
+
+# prefix is the first input taken
+prefix="$1"
+
+#Verify Input length
+[ "${#prefix}" -lt 5 ] && \
+printf "Prefix length is too short\nPrefix example: 10.0.17\n" && \
+exit 1
+
+for i in {1..255}
+do
+	ping -c 1 "$prefix.$i" | grep  "64 bytes from" | \
+	grep -o -E "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}"
+done
