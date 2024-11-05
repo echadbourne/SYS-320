a643681f4789d2d58d66aab3c1adcfae1d6c592b
echadbourne <elizabeth.chadbourne@mymail.champlain.edu>
2024-11-05 10:56:02 -0500

    all week 10 things done

--- week10/pageCount.bash
+++ week10/pageCount.bash
@@ -4,8 +4,14 @@ allLogs=""
 file="/var/log/apache2/access.log"
 
 function pageCount(){
-allLogs=$(cat "$file" | cut -d' ' -f7 | sort | uniq -c )
+allLogs=$(cat "$file" | cut -d' ' -f1,12 | sort | uniq -c )
+}
+
+function countingCurlAccess(){
+curlAccess=$(echo "$allLogs"  | grep "curl" )
 }
 
 pageCount
-echo "$allLogs"
+countingCurlAccess
+#echo "$allLogs"
+echo "$curlAccess"
